884989a7334d814e2f125059c784343c05398bab
Asan Abdurakhmanov <asan11@outlook.com>
2019-05-26 14:14:51 +0300

    update api function

--- src/helpers.php
+++ src/helpers.php
@@ -3,12 +3,6 @@
 if (!function_exists('api')) {
     function api()
     {
-        static $api;
-
-        if (!$api) {
-            $api = new \Kolirt\ApiResponse\Api;
-        }
-
-        return $api;
+        return new \Kolirt\ApiResponse\Api;
     }
 }
